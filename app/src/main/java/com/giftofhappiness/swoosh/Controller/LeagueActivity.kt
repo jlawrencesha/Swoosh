33028913ed4a3ef4ff398a0c26d7d19ad9bee98e
jlawrencesha <44271286+jlawrencesha@users.noreply.github.com>
2020-05-19 13:08:11 -0700

    created a class for player in order to share player custom properties with other activities rather than individual variables.

--- app/src/main/java/com/giftofhappiness/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/giftofhappiness/swoosh/Controller/LeagueActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.giftofhappiness.swoosh.Utilities.EXTRA_LEAGUE
+import com.giftofhappiness.swoosh.Model.Player
 import com.giftofhappiness.swoosh.R
+import com.giftofhappiness.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+   // var selectedLeague = ""
+    var player = Player("", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,28 +25,28 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         coedLeagueBtn.isChecked =false
 
-        selectedLeague = "mens"
+        player.league = "mens"
 
     }
     fun onWomensClick(view: View){
         mensLeagueBtn.isChecked=false
         coedLeagueBtn.isChecked = false
 
-        selectedLeague = "womens"
+        player.league = "womens"
 
     }
     fun onCoedClick(view: View){
         mensLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.league = "co-ed"
     }
 
     fun leagueNextClicked(view: View){
 
-        if(selectedLeague != "") {
+        if(player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         }else{
             Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
